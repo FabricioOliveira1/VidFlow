b3b87e1ac3c32bd2fa608140650bf2a5f3d34b10
Fabricio Oliveira <fabricioliveira021@gmail.com>
2024-07-10 23:24:12 -0300

    Foi desenvolvido funcionamento da barra de pesquisa e filtros.

--- script.js
+++ script.js
@@ -11,9 +11,10 @@ async function buscarEmostrarVideos() {
         <li class="videos__item">
           <iframe src="${video.url}" title="${video.titulo}" frameborder="0" alloefullscreen></iframe>
           <div class="descricao-video">
-            <img class="img-canal" src="${video.imagem} alt="Logo do Canal"
+            <img class="img-canal" src="${video.imagem} alt="Logo do Canal">
             <h3 class="titulo-video">${video.titulo}<h3>
             <p class="titulo-canal">${video.descricao}</p>
+            <p class="categoria" hidden>${video.categoria}</p>
         </li>
         `;
       
@@ -24,3 +25,48 @@ async function buscarEmostrarVideos() {
 }
 
 buscarEmostrarVideos();
+
+const barraDePesquisa = document.querySelector(".pesquisar__input")
+
+barraDePesquisa.addEventListener("input", filtrarPesquisa)
+
+function filtrarPesquisa(){
+  const videos = document.querySelectorAll(".videos__item");
+
+  if(barraDePesquisa.value != ""){
+      for(let video of videos){
+          let titulo = video.querySelector(".titulo-video").textContent.toLowerCase();
+          let valorFiltro = barraDePesquisa.value.toLowerCase();
+
+          if(!titulo.includes(valorFiltro)){
+              video.style.display = "none";
+          } else {
+              video.style.display = "block";
+          }
+
+      }
+    } else {
+      video.style.display = 'block';
+    }
+  }
+
+  const botaoCategoria = document.querySelectorAll(".superior__item")
+
+  botaoCategoria.forEach((botao) => {
+    let nomeCategoria = botao.getAttribute("name")
+    botao.addEventListener("click", () => filtrarPorCategoria(nomeCategoria))
+  })
+
+  function filtrarPorCategoria(filtro) {
+    const videos = document.querySelectorAll(".videos__item")
+    for(let video of videos){
+      let categoria = video.querySelector(".categoria").textContent.toLowerCase()
+      let valorFiltro = filtro.toLowerCase()
+
+      if(!categoria.includes(valorFiltro) && valorFiltro != 'tudo'){
+        video.style.display = 'none';
+      } else {
+        video.style.display = 'block';
+      }
+    }
+  }
